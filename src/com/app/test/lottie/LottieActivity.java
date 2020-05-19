140573768f17492c19fdb2ee9a7e65c220d4e66d
jam <bjlcx521@sina.com>
2020-05-19 10:06:04 +0800

    ....

--- src/com/app/test/lottie/LottieActivity.java
+++ src/com/app/test/lottie/LottieActivity.java
@@ -1,7 +1,5 @@
 package com.app.test.lottie;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,7 +7,6 @@ import android.view.View;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.app.test.R;
-import com.app.test.util.Utils;
 
 /**
  * @author lcx
@@ -44,55 +41,13 @@ public class LottieActivity extends Activity {
             }
         });
 
-        ivLottie.useHardwareAcceleration(true);
-        ivLottie.setImageAssetsFolder("drink/images");
-        ivLottie.setAnimation("drink/drink_water.json");
-
-        ivLottie1.useHardwareAcceleration(true);
-        ivLottie1.setImageAssetsFolder("gld/images");
-        ivLottie1.setAnimation("gld/loading.json");
-
-        ivLottie3.useHardwareAcceleration(true);
-        ivLottie3.setImageAssetsFolder("back/images");
-        ivLottie3.setAnimation("back/maopu.json");
 
 
     }
 
     private void startAnim() {
-        ivLottie.playAnimation();
-        ivLottie.removeAllAnimatorListeners();
-        ivLottie.addAnimatorListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                drinkWaterEnd();
-            }
-        });
 
-        ivLottie1.playAnimation();
-        ivLottie1.removeAllAnimatorListeners();
-        ivLottie1.addAnimatorListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-            }
-        });
-
-        ivLottie3.playAnimation();
-        ivLottie3.setRepeatCount(-1);
-        ivLottie3.removeAllAnimatorListeners();
-        ivLottie3.addAnimatorListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-            }
-        });
     }
 
-    public void drinkWaterEnd() {
-        if (!Utils.isEmpty(ivLottie)) {
-            ivLottie.setProgress(0);
-        }
-    }
+
 }
