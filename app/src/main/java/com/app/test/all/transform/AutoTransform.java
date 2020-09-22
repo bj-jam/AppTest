0e6a4ea1d25f77af42a2f8bf3b18e2c202d91bcb
jam <bjlcx521@sina.com>
2020-09-22 09:40:45 +0800

    工具

--- app/src/main/java/com/app/test/all/transform/AutoTransform.java
+++ app/src/main/java/com/app/test/all/transform/AutoTransform.java
@@ -1,33 +1,33 @@
-package com.app.test.all.transform;
-
-import com.android.build.api.transform.QualifiedContent;
-import com.android.build.api.transform.Transform;
-
-import java.util.Set;
-
-/**
- * @author lcx
- * Created at 2020.9.14
- * Describe:
- */
-public class AutoTransform extends Transform {
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public Set<QualifiedContent.ContentType> getInputTypes() {
-        return null;
-    }
-
-    @Override
-    public Set<? super QualifiedContent.Scope> getScopes() {
-        return null;
-    }
-
-    @Override
-    public boolean isIncremental() {
-        return false;
-    }
-}
+//package com.app.test.all.transform;
+//
+//import com.android.build.api.transform.QualifiedContent;
+//import com.android.build.api.transform.Transform;
+//
+//import java.util.Set;
+//
+///**
+// * @author lcx
+// * Created at 2020.9.14
+// * Describe:
+// */
+//public class AutoTransform extends Transform {
+//    @Override
+//    public String getName() {
+//        return null;
+//    }
+//
+//    @Override
+//    public Set<QualifiedContent.ContentType> getInputTypes() {
+//        return null;
+//    }
+//
+//    @Override
+//    public Set<? super QualifiedContent.Scope> getScopes() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean isIncremental() {
+//        return false;
+//    }
+//}
